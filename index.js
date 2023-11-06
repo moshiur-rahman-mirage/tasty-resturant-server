5c195b914c7f9475690ce57ff3e89c84d579e52c
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-11-07 00:20:28 +0600

    created  menu and order api

---
+++ index.js
@@ -0,0 +1,173 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors({
+    origin: [
+         'http://localhost:5173',
+        //'https://cars-doctor-6c129.web.app',
+        //'https://cars-doctor-6c129.firebaseapp.com'
+    ],
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+
+console.log(process.env.DB_PASS)
+
+const uri = `mongodb+srv://librarian:Ms121212@cluster0.hhabjy4.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+// middlewares 
+const logger = (req, res, next) =>{
+    console.log('log: info', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) =>{
+    const token = req?.cookies?.token;
+    if(!token){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const menuCollection = client.db('Resturant').collection('Menu');
+        const orderCollection = client.db('Resturant').collection('Order');
+      
+
+        // auth related api
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body;
+            console.log('user for token', user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            })
+                .send({ success: true });
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            console.log('logging out', user);
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
+
+        // services related api
+        app.get('/manu', async (req, res) => {
+            const cursor = menuCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/menu/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
+            };
+
+            const result = await menuCollection.findOne(query, options);
+            res.send(result);
+        })
+
+        app.post('/menu', async (req, res) => {
+            const menu = req.body;
+            console.log(menu);
+            const result = await menuCollection.insertOne(menu);
+            res.send(result);
+        });
+
+
+        // orders 
+        app.get('/orders', logger, verifyToken, async (req, res) => {
+            if(req.user.email !== req.query.email){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await orderCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            console.log(order);
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedorder = req.body;
+            console.log(updatedorder);
+            const updateDoc = {
+                $set: {
+                    status: updatedorder.status
+                },
+            };
+            const result = await orderCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('doctor is running')
+})
+
+app.listen(port, () => {
+    console.log(`Car Doctor Server is running on port ${port}`)
+})
