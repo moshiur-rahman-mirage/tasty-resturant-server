26efb6502d1354adaf897cd56cec7c420050db0f
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-11-09 22:11:45 +0600

    trying

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ app.use(
 )
 
 
-// app.use(express.json());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json());
+// app.use(express.json({limit: '50mb'}));
+// app.use(express.urlencoded({limit: '50mb'}));
 
 app.use(cookieParser());
 
