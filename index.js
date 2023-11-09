3bda6e836d5c9f6f89cd022452cb521cd760b78b
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-11-09 21:57:11 +0600

    cors ussue happeing sometimes after deploy

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const sharp = require("sharp");
 
 const corsOptions ={
     origin:['https://resturant-9e927.web.app', 
-    'http://localhost:5173','b8a11-server-side-moshiur-rahman-mirage.vercel.app'],
+    'http://localhost:5173','https://b8a11-server-side-moshiur-rahman-mirage.vercel.app'],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
 
