8a623b798c818eab13059dc85dba522f3d9b7192
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-11-09 22:42:32 +0600

    hello

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ async function run() {
         app.post('/jwt', logger, async (req, res) => {
             const user = req.body;
             console.log('user for token', user);
-            const token = jwt.sign(user, secret, { expiresIn: '1h' });
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 
             res.cookie('token', token, {
                 httpOnly: true,
